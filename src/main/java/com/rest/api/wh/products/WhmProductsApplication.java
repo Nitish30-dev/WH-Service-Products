bf4468ad507ed66afba37cb8382d6925e0bee393
Nitish Kumar <09novnitishk@gmail.com>
2021-12-15 08:58:44 +0530

    First code commit

---
+++ src/main/java/com/rest/api/wh/products/WhmProductsApplication.java
@@ -0,0 +1,13 @@
+package com.rest.api.wh.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhmProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhmProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/api/wh/products/WhmProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.api.wh.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhmProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
